7143ee3eb0afb1ae2dfa9942bd4d54121985fa24
NikolaiT1986 <kolyan-04031986@mail.ru>
2025-04-14 00:53:36 +0300

    refactor: UnitTests 1. remove useless defaultUnitTest 2. add javadoc

--- src/test/java/org/nikolait/assignment/caloriex/UnitTests.java
+++ src/test/java/org/nikolait/assignment/caloriex/UnitTests.java
@@ -1,17 +1,11 @@
 package org.nikolait.assignment.caloriex;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
+/**
+ * Base class for running all unit tests with MockitoExtension
+ */
 @ExtendWith(MockitoExtension.class)
 public abstract class UnitTests {
-
-    @Test
-    void defaultUnitTest() {
-        assertTrue(true, "This is the base class for running all unit tests");
-    }
-
 }
